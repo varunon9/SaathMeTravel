3c5b115b76a5dcc3e712b1fbff9d91383211d084
varunon9 <varunon9@gmail.com>
2018-10-02 22:16:02 +0530

    integrated google place APIs

--- app/src/main/java/me/varunon9/saathmetravel/JourneyPlannerActivity.java
+++ app/src/main/java/me/varunon9/saathmetravel/JourneyPlannerActivity.java
@@ -5,9 +5,25 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
+
+import me.varunon9.saathmetravel.constants.AppConstants;
+
+import static com.google.android.gms.location.places.AutocompleteFilter.TYPE_FILTER_ADDRESS;
 
 public class JourneyPlannerActivity extends AppCompatActivity {
+
+    private String TAG = "JourneyPlannerActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,64 @@ public class JourneyPlannerActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        PlaceAutocompleteFragment sourceAutocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.source_autocomplete_fragment);
+        PlaceAutocompleteFragment destinationAutocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.destination_autocomplete_fragment);
+
+        // setting filter
+        AutocompleteFilter autocompleteFilter = new AutocompleteFilter.Builder()
+                .setTypeFilter(TYPE_FILTER_ADDRESS)
+                .build();
+        sourceAutocompleteFragment.setFilter(autocompleteFilter);
+        destinationAutocompleteFragment.setFilter(autocompleteFilter);
+
+        // changing hint and icon
+        sourceAutocompleteFragment.setHint(
+                getResources().getString(R.string.enter_source_hint_text)
+        );
+        destinationAutocompleteFragment.setHint(
+                getResources().getString(R.string.enter_destination_hint_text)
+        );
+
+        ImageView sourceSearchIcon = (ImageView)((LinearLayout)sourceAutocompleteFragment
+                .getView()).getChildAt(0);
+        sourceSearchIcon.setImageResource(R.drawable.ic_time_to_leave);
+
+        ImageView destinationSearchIcon = (ImageView)((LinearLayout)destinationAutocompleteFragment
+                .getView()).getChildAt(0);
+        destinationSearchIcon.setImageResource(R.drawable.ic_destination);
+
+
+        sourceAutocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                String placeAddress = place.getAddress().toString();
+                LatLng latlng = place.getLatLng();
+                Log.i(TAG, "Place: " + placeAddress);
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.i(TAG, "An error occurred: " + status);
+                showMessage(AppConstants.GENERIC_ERROR_MESSAGE + " Status: " + status);
+            }
+        });
+        destinationAutocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                String placeAddress = place.getAddress().toString();
+                LatLng latlng = place.getLatLng();
+                Log.i(TAG, "Place: " + placeAddress);
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.i(TAG, "An error occurred: " + status);
+                showMessage(AppConstants.GENERIC_ERROR_MESSAGE + " Status: " + status);
+            }
+        });
     }
 
     @Override
@@ -25,4 +99,8 @@ public class JourneyPlannerActivity extends AppCompatActivity {
         return true;
     }
 
+    private void showMessage(String message) {
+        View parentLayout = findViewById(R.id.activityContent);
+        Snackbar.make(parentLayout, message, Snackbar.LENGTH_LONG).show();
+    }
 }
