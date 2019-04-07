4b9649d90540d91f94d82bad168448b8ebe8718b
varunon9 <varunon9@gmail.com>
2019-04-07 18:07:55 +0530

    added /extranet/feedback api

--- server/app.js
+++ server/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json({
+  limit: '8mb'
+})); // support json encoded bodies
+
+app.use(bodyParser.urlencoded({
+  limit: '8mb',
+  extended: true
+})); // support encoded bodies
 
 const logger = require('./modules/logger');
 
@@ -13,6 +24,14 @@ app.use(function(req, res, next) {
 
 app.get('/', function(req, res) {
   res.redirect('https://github.com/varunon9/SaathMeTravel');
+});
+
+app.post('/extranet/feedback', function(req, res) {
+  logger.info(req.body);
+  res.json({
+    success: true,
+    message: 'Thank you for your feedback.'
+  });
 });
 
 module.exports = app;
